473d8b6384d6bed75ffeef7d96b5657abe7c57ab
vivek <vivek@dhrusoft.com>
2022-10-14 17:30:43 +0530

    - Default input amount open for item unit - Default input value

--- app/src/pages/Navigation/DrawerNavigatorContent.tsx
+++ app/src/pages/Navigation/DrawerNavigatorContent.tsx
@@ -24,6 +24,7 @@ const Index = () => {
         navigation.navigate("PinStackNavigator");
     }
 
+
     return <View style={[styles.h_100, styles.px_5]}>
         {/*<Card style={[styles.card]}>*/}
         {/*    <Card.Content>*/}
